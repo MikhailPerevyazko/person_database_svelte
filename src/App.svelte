b520c6ff5ec1eef99a61fe459c43bb867b6190d4
Mikhail Perevyazko <perevyazko.misha2001@gmail.com>
2024-03-14 08:38:16 +0300

    add direction components

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import SelectFileButton from './lib/components/InputsOutputs/SelectFileButton.svelte'
+  import SelectFileButton from './lib/components/Buttons/SelectFileButton.svelte'
   import DeleteButton from './lib/components/Buttons/DeleteButton.svelte';
   import FindByIdButton from './lib/components/Buttons/FindByIdButton.svelte';
   import AddPersonInfo from './lib/components/InputsOutputs/AddPersonInfo.svelte';
@@ -10,11 +10,11 @@
   <div class="m-10 grid grid-rows-10 gap-4">
     <div class="header space-y-4 border-2 border-emerald-600 rows-span-2 m-10">
       <h1 class="mb-4">FIND PERSON</h1>
-        <SelectFileButton/>
-        <input class="ml-4 rounded-lg" type="text" placeholder="Enter the ID">
-        <FindByIdButton/>
-        <DeleteButton/>
-        <AddPersonInfo/>
+      <SelectFileButton/>
+      <input class="ml-4 rounded-lg" type="text" placeholder="Enter the ID">
+      <FindByIdButton/>
+      <DeleteButton/>
+      <AddPersonInfo/>
     </div>
     <div class="rows-span-6">
       <OutputInfo></OutputInfo>
@@ -23,7 +23,7 @@
 </main>
 
 <style>
-  /* .header {
+   /* .header {
     position: fixed;
-  } */
+  }  */
 </style>
--- src/lib/components/InputsOutputs/AddPersonInfo.svelte
+++ src/lib/components/InputsOutputs/AddPersonInfo.svelte
@@ -3,38 +3,38 @@ import { AccordionItem, Accordion } from 'flowbite-svelte';
 import { Label, Input } from 'flowbite-svelte';
 </script>
 
-<div class="m-4">
-    <Accordion >
-      <AccordionItem>
-        <span slot="header">Add new info</span>
-        <div class="mb-6">
-          <Label for="large-input" class="block mb-2">Name</Label>
-          <Input id="large-input" size="lg" placeholder="Input new person's name" />
-        </div>
-        <div class="mb-6">
-          <Label for="large-input" class="block mb-2">Surname</Label>
-          <Input id="large-input" size="lg" placeholder="Input new person's surname" />
-        </div>
-        <div class="mb-6">
-          <Label for="large-input" class="block mb-2">Middle name</Label>
-          <Input id="large-input" size="lg" placeholder="Input new person's middle name" />
-        </div>
-        <div class="mb-6">
-          <Label for="large-input" class="block mb-2">Date of birth</Label>
-          <Input id="large-input" size="lg" placeholder="Input new person's date of birth" />
-        </div>
-        <div class="mb-6">
-          <form class="">
-            <Label for="large-input" class="block">Gender</Label>
-            <select id="underline_select">
-                <option value="true">Male</option>
-                <option value="false">Female</option>
-            </select>
-          </form>
-          <button class="m-4 text-white bg-green-700 justify-center">Add new info</button>
-        </div>
-      </AccordionItem>
-    </Accordion>
+<div>
+  <Accordion class="m-4">
+    <AccordionItem>
+      <span slot="header">Add new info</span>
+      <div class="mb-6">
+        <Label for="large-input" class="block mb-2">Name</Label>
+        <Input id="large-input" size="lg" placeholder="Input new person's name" />
+      </div>
+      <div class="mb-6">
+        <Label for="large-input" class="block mb-2">Surname</Label>
+        <Input id="large-input" size="lg" placeholder="Input new person's surname" />
+      </div>
+      <div class="mb-6">
+        <Label for="large-input" class="block mb-2">Middle name</Label>
+        <Input id="large-input" size="lg" placeholder="Input new person's middle name" />
+      </div>
+      <div class="mb-6">
+        <Label for="large-input" class="block mb-2">Date of birth</Label>
+        <Input id="large-input" size="lg" placeholder="Input new person's date of birth" />
+      </div>
+      <div class="mb-6">
+        <form class="">
+          <Label for="large-input" class="block">Gender</Label>
+          <select id="underline_select">
+            <option value="true">Male</option>
+            <option value="false">Female</option>
+          </select>
+        </form>
+        <button class="m-4 text-white bg-green-700 justify-center">Add new info</button>
+      </div>
+    </AccordionItem>
+  </Accordion>
 </div>
   
 <style></style>
